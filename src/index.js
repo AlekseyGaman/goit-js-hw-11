2935b937aa029cd7e7a6b12d2fbcc5c4dd747ec4
AlekseyGaman <4117988@gmail.com>
2022-10-28 01:10:15 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,16 +1,95 @@
-import './css/styles.css';
+import './scss/main.scss';
+import fetchImages from './js/fetch';
+import cardTemplate from './templ/template.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// import throttle from 'lodash.throttle';
 
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import debounce from 'lodash.debounce';
-
-const refs = {
-  searchForm: document.querySelector('#search-form'),
-  input: document.querySelector('form.input'),
-  button: document.querySelector('form.button'),
+const { searchForm, gallery, loadMoreBtn, endCollectionText } = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  endCollectionText: document.querySelector('.end-collection-text'),
 };
 
-const DEBOUNCE_DELAY = 300;
-refs.searchForm.addEventListener(
-  'input',
-  debounce(onInputCountry, DEBOUNCE_DELAY)
-);
+function renderCardImage(arr) {
+  const markup = arr.map(item => cardTemplate(item)).join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+let lightbox = new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+let currentPage = 1;
+let currentHits = 0;
+let searchQuery = '';
+
+searchForm.addEventListener('submit', onSubmitSearchForm);
+
+async function onSubmitSearchForm(e) {
+  e.preventDefault();
+  searchQuery = e.currentTarget.searchQuery.value;
+  currentPage = 1;
+
+  if (searchQuery === '') {
+    return;
+  }
+
+  const response = await fetchImages(searchQuery, currentPage);
+  currentHits = response.hits.length;
+
+  if (response.totalHits > 40) {
+    loadMoreBtn.classList.remove('is-hidden');
+  } else {
+    loadMoreBtn.classList.add('is-hidden');
+  }
+
+  try {
+    if (response.totalHits > 0) {
+      Notify.success(`Hooray! We found ${response.totalHits} images.`);
+      gallery.innerHTML = '';
+      renderCardImage(response.hits);
+      lightbox.refresh();
+      endCollectionText.classList.add('is-hidden');
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * -100,
+        behavior: 'smooth',
+      });
+    }
+
+    if (response.totalHits === 0) {
+      gallery.innerHTML = '';
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      loadMoreBtn.classList.add('is-hidden');
+      endCollectionText.classList.add('is-hidden');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+loadMoreBtn.addEventListener('click', onClickLoadMoreBtn);
+
+async function onClickLoadMoreBtn() {
+  currentPage += 1;
+  const response = await fetchImages(searchQuery, currentPage);
+  renderCardImage(response.hits);
+  lightbox.refresh();
+  currentHits += response.hits.length;
+
+  if (currentHits === response.totalHits) {
+    loadMoreBtn.classList.add('is-hidden');
+    endCollectionText.classList.remove('is-hidden');
+  }
+}
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,28 +1,9 @@
-// export function fetchCountries(name) {
-//   const url = 'https://pixabay.com/api/';
-//   const filter = '?fields=name,capital,population,flags,languages';
-//   const KEY = 30833881 - '30833881-2a0b5d994d787ba4a8b90c425';
-//   return fetch(`${url}${name}${filter}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
+import axios from 'axios';
 
-//     return response.json();
-//   });
-// }
+export default async function fetchImages(value, page) {
+  const url = 'https://pixabay.com/api/';
+  const key = '30833881-2a0b5d994d787ba4a8b90c425';
+  const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
-export function fetchImages(name) {
-  var API_KEY = '30833881-2a0b5d994d787ba4a8b90c425';
-  var URL =
-    'https://pixabay.com/api/?key=' +
-    API_KEY +
-    '&q=' +
-    encodeURIComponent('red roses');
-  $.getJSON(URL, function (data) {
-    if (parseInt(data.totalHits) > 0)
-      $.each(data.hits, function (i, hit) {
-        console.log(hit.pageURL);
-      });
-    else console.log('No hits');
-  });
+  return await axios.get(`${url}${filter}`).then(response => response.data);
 }
